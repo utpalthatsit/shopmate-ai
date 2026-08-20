88224206c3a7463520e27e645d009b1182c3a073
utpal kumar yadav <utpalyadav2110@gmail.com>
2026-08-20 17:28:52 +0530

    Set Supabase project URL

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'PASTE_YOUR_SUPABASE_URL_HERE';
+const SUPABASE_URL = 'https://ntknopebuiskpdlmhqbq.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_8Be5kHYMRkPRqKRXbQX4Ew_EkvVHnYE';
 
 const supabase = window.supabase.createClient(
